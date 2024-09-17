7c80bdb7a8849698da62269f30aede2b7b7ac2ea
ARamos <albertramos902@gmail.com>
2024-09-17 15:40:41 +0000

    creating elements of the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-      <div className="">Hello world</div>
+      <div className="text-sky-600">Hello world</div>
   
       
   );
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { BsBellFill, BsHouseFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+
+const items = [
+  {
+    label: "Home",
+    href: "/",
+    icon: BsHouseFill,
+  },
+  {
+    label: "Notifications",
+    href: "/notifications",
+    icon: BsBellFill,
+  },
+  {
+    label: "Profile",
+    href: "/users/123",
+    icon: FaUser,
+  },
+];
+
 
 const Sidebar = () => {
   return (
     <div>
-      
+
     </div>
   )
 }
