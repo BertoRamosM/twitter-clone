df46a61ac90889433e49695ad223723c054f2492
ARamos <albertramos902@gmail.com>
2024-09-18 13:51:36 +0000

    created register modal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,15 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 import React, { useCallback, useState } from 'react'
 import Input from '../layout/Input'
 import Modal from '../Modal'
 import useRegisterModal from '@/hooks/useRegisterModal'
+import useLoginModal from '@/hooks/useLoginModal'
 
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
   const [isLoading, setIsLoading] = useState(false)
 
   const onSubmit = useCallback(async () => {
@@ -34,6 +39,18 @@ const RegisterModal = () => {
         disabled={isLoading}
       />
       <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
         placeholder="Password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
@@ -43,15 +60,23 @@ const RegisterModal = () => {
     </div>
   );
 
+
+  const footerContent = (
+    <div className="text-neutral-400 text-center mt-4">
+      <p>Already have an account? <span className="text-white hover:underline cursor-pointer" onClick={loginModal.onOpen}>Sign in</span></p>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
       isOpen={registerModal.isOpen}
       onClose={registerModal.onClose}
       onSubmit={onSubmit}
-      title="Login"
+      title="Create an account"
       body={bodyContent}
-      actionLabel="Login"
+      actionLabel="Register"
+      footer={footerContent}
     />
   );
 }
