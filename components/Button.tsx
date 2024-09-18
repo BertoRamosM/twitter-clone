acdf126af3324a8acfc58165f8c7ede805726f44
ARamos <albertramos902@gmail.com>
2024-09-18 08:08:15 +0000

    finished button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,9 +12,15 @@ interface ButtonProps {
 const Button = ({ label, secondary, fullWidth, large, onClick, disabled, outline }: ButtonProps) => {
   
   return (
-    <div>
-
-    </div>
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 ${fullWidth ? `w-full` : `w-fit`}
+    ${secondary ? `bg-white text-black border-black` : `bg-sky-500 text-white border-sky-500`} ${large ? "text-xl p-5 py-4" : "text-md px-4 py-2"}
+    ${outline ? `bg-transparent border-white text-white` : ``}
+    `}>
+      {label}
+    </button>
   )
 }
 
